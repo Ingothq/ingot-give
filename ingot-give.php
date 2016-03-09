4f8f1fa355fd5990070df8b929ac0866277ca308
Josh Pollock <jpollock412@gmail.com>
2016-03-08 23:33:00 -0500

    Freemius FTW

--- ingot-give.php
+++ ingot-give.php
@@ -23,8 +23,46 @@ add_action( 'ingot_before', function(){
 	if( ! defined( 'INGOT_GIVE_VER' ) ) {
 		define( 'INGOT_GIVE_VER', '1.0.0' );
 		require_once dirname( __FILE__ ) . '/vendor/autoload.php';
-		new add_destinations();
-		new tracking();
+
+		/**
+		 * Freemius setup
+		 *
+		 * @since 1.0.0
+		 *
+		 * @return \Freemius
+		 */
+		function ingot_give_fs() {
+			global $ingot_give_fs;
+
+			if ( ! isset( $ingot_give_fs ) ) {
+				$ingot_give_fs = fs_dynamic_init( [
+					'id'             => '225',
+					'slug'           => 'ingotgive',
+					'public_key'     => 'pk_24d588a393396695fcbc003bb5d30',
+					'is_premium'     => true,
+					'has_paid_plans' => true,
+					'parent'         => [
+						'id'         => '210',
+						'slug'       => 'ingot',
+						'public_key' => 'pk_e6a19a3508bdb9bdc91a7182c8e0c',
+						'name'       => 'Ingot',
+					]
+				] );
+			}
+
+			return $ingot_give_fs;
+		}
+
+		/**
+		 * Boot Freemius integration
+		 */
+		add_action( 'ingot_loaded', 'ingot_give_fs', 25 );
+
+		add_action( 'ingot_loaded', function () {
+			new add_destinations();
+			new tracking();
+		}, 26 );
+
 	}
 
 });
